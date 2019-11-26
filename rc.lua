93eb5d1c755fbca3eed33195b2372489bc9b9467
Francesc <francesc.arpi@gmail.com>
2019-11-26 07:14:38 +0100

    Testing some compton configurations

--- rc.lua
+++ rc.lua
@@ -15,6 +15,9 @@ awful.util.spawn("setxkbmap -layout es")
 require("awful.autofocus")
 require("awful.hotkeys_popup.keys.vim")
 
+home = os.getenv("HOME")
+
+
 --------------------------------------------------------------------------------
 -- Error Handling
 --------------------------------------------------------------------------------
@@ -655,6 +658,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 awful.util.spawn("nm-applet")
 awful.util.spawn("blueman-applet")
 awful.util.spawn("flameshot")
-awful.util.spawn("compton")
+awful.util.spawn("picom --config " .. home .. "/.config/awesome/compton.conf")
 awful.util.spawn("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 
